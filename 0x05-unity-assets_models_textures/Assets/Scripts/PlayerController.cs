6a54afec0310c12f3bb059fec9998ebf67c4d010
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-09-02 11:52:52 +0100

    3. Jump man

---
+++ 0x05-unity-assets_models_textures/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Controls player speed
+    public float speed = 10.0f;
+    // Rigidbody of the player
+    private Rigidbody rb;
+    // Force of player jump
+    public float jumpForce = 16.0f;
+    // Checks if the player is grounded or not (used for jump)
+    private bool isGrounded;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        isGrounded = true;
+    }
+
+    // Update is called each frame
+    void Update(){
+        if (Input.GetKeyDown(KeyCode.Space)){
+            if (isGrounded){
+                rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            }
+        }        
+           
+    }
+
+    // When the player collide with another object
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.transform.parent.name == "Platforms"){
+            isGrounded = true;
+        }
+    }
+    
+    // When the player gets out of a collission
+    void OnCollisionExit(Collision collision) {
+            isGrounded = false;
+    }
+
+    // Fixedupdate is called according to framerate
+    void FixedUpdate()
+    {
+        float translationX = Input.GetAxis("Horizontal") * speed;
+        float translationZ = Input.GetAxis("Vertical") * speed;
+
+        translationX *= Time.deltaTime;
+        translationZ *= Time.deltaTime;
+
+        if (translationX != 0 || translationZ != 0){
+            transform.Translate(translationX, 0, translationZ);
+        }
+        if (!isGrounded){
+            Vector3 newVel = rb.velocity;
+            newVel.y -= (jumpForce * 2) * Time.deltaTime;
+            rb.velocity = newVel;
+        }
+            
+    }
+}
