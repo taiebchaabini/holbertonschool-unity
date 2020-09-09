17438230c1319a7a1bfdff0dee9848965bed94fb
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-09-09 22:46:24 +0100

    All complete

--- 0x05-unity-assets_models_textures/Assets/Scripts/PlayerController.cs
+++ 0x05-unity-assets_models_textures/Assets/Scripts/PlayerController.cs
@@ -16,7 +16,9 @@ public class PlayerController : MonoBehaviour
     public float jumpForce = 16.0f;
     // Checks if the player is grounded or not (used for jump)
     private bool isGrounded;
+    Vector3 direction;
 
+   
 
     // Start is called before the first frame update
     void Start()
@@ -28,32 +30,30 @@ public class PlayerController : MonoBehaviour
 
     // Update is called each frame
     void Update(){
-        if (transform.position.y < -20){
+        direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        if (transform.position.y < -20f){
             transform.position = (new Vector3(0,20f,0));
             transform.rotation = Quaternion.identity;
+            isGrounded = false;
         }
-        if (Input.GetKeyDown(KeyCode.Space)){
+      
+        if (isGrounded == true && Input.GetKeyDown(KeyCode.Space)){
+            Debug.Log("Space used");
             if (isGrounded){
                 rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             }
-        }        
+        }
+         
+ 
            
     }
 
 
     // FixedUpdate is called according to framerate
-    void FixedUpdate()
-    {
-        float translationX = Input.GetAxis("Horizontal") * speed;
-        float translationZ = Input.GetAxis("Vertical") * speed;
-
-
-        translationX *= Time.deltaTime;
-        translationZ *= Time.deltaTime;
-
-        if (translationX != 0 || translationZ != 0){
-            transform.Translate(translationX, 0, translationZ);
-        }
+    void FixedUpdate(){
+        MoveCharacter(direction);
+      
         if (!isGrounded){
             Vector3 newVel = rb.velocity;
             newVel.y -= (jumpForce * 2) * Time.deltaTime;
@@ -62,11 +62,16 @@ public class PlayerController : MonoBehaviour
             
     }
 
-
+    void MoveCharacter(Vector3 movement){
+        //rb.rotation = CameraController.GameObject.GetComponent<CameraController>().transform.localRotation;
+        movement = transform.TransformDirection(movement);
+        rb.MovePosition(transform.position + (movement * speed * Time.deltaTime));
+    }
     // Fired when the player collide with another object
     void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.transform.parent.name == "Platforms"){
+
+        if (isGrounded == false){
             isGrounded = true;
         }
     }
@@ -74,8 +79,11 @@ public class PlayerController : MonoBehaviour
 
     // Fired when the player gets out of a collission
     void OnCollisionExit(Collision collision) {
+   
             isGrounded = false;
     }
+
+
 
   
 }
