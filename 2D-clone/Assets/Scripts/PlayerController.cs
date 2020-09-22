2a60121a00f08383f70316b7ed2ed6846f32d21d
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-09-22 23:51:28 +0100

    New files

---
+++ 2D-clone/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    /// <summary>
+    /// Player speed
+    /// </summary>
+    public float speed;
+
+    /// <summary>
+    /// Handles player animations.
+    /// </summary>
+    public Animator animator;
+    /// <summary>
+    /// Player rigidbody 2D.
+    /// </summary>
+    public Rigidbody2D rb;
+    // Handles player Horizontal input
+    private float move;
+    // Handles if the player jumped or not.
+    private float jump;
+    // Old rotation values
+    private Quaternion oldRotation;
+    // Previous transform position
+    private Vector3 previous;
+    // Actual velocity, based on TimedeltaTime;
+    private float velocity;
+    // checks if the player is grounded
+
+    private bool isGrounded;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        move = 10f;
+        isGrounded = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move = Input.GetAxis("Horizontal");
+        jump = Input.GetAxis("Jump");
+    }
+
+    void FixedUpdate(){
+        // FixedUpdate is called depending on thed computer
+              
+        if (move < 0){
+            transform.rotation = Quaternion.Euler(0, -180, 0);
+        }
+        else if (move > 0){
+            transform.rotation = Quaternion.identity;
+        }
+        
+        /*
+        //Debug.Log($"Actual rotation: {transform.rotation.y} vs {oldRotation.y}");
+        if (velocity > 0 && oldRotation.y != transform.rotation.y){
+        
+            StartCoroutine(StartSkid());
+            
+            Debug.Log("Skied on");
+        } 
+        oldRotation = transform.rotation;
+        */
+
+        if (!animator.GetBool("PlayerSkid")){
+            animator.SetFloat("Horizontal", move);
+        }
+
+        if (animator.GetBool("Skid") == false && move != 0){
+            //rb.MovePosition(transform.position + new Vector3(move, 0, 0) * Time.deltaTime * speed);
+            transform.Translate(Vector2.right * move * Time.deltaTime * speed, 0);
+        }
+
+        if (isGrounded == true && jump != 0){
+             rb.AddForce((new Vector2(0,1) * 7), ForceMode2D.Impulse);
+        }
+        velocity = ((transform.position - previous).magnitude) / Time.deltaTime;
+        previous = transform.position;
+
+    }
+
+    // Handles skid animation
+    IEnumerator StartSkid(){
+        animator.SetTrigger("PlayerSkid");
+        animator.SetBool("Skid", true);
+    
+        yield return new WaitForSeconds(0.5f);
+
+        animator.SetBool("Skid", false);
+    }
+
+    // Handles player collision
+    void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log("Exit");
+        isGrounded = false;
+    }
+    // Handles player collision
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Enter");
+        isGrounded = true;
+    }
+
+
+    
+
+
+}
