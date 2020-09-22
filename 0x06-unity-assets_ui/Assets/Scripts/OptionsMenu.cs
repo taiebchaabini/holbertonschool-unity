e989d064546c82b375e8c0870554715457d55dce
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-09-22 19:33:59 +0100

    Task 4

---
+++ 0x06-unity-assets_ui/Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OptionsMenu : MonoBehaviour
+{
+    /// <summary>
+    /// Loads the last scene
+    /// </summary>
+    public void Back(){
+        var lastScene = PlayerPrefs.GetString("__lastScene__");
+        SceneManager.LoadScene(lastScene);
+    }
+}
