760883a18793907a994cb8c4fdd5045791b102f0
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-09-22 20:37:29 +0100

    Task 4

--- 0x06-unity-assets_ui/Assets/Scripts/PauseMenu.cs
+++ 0x06-unity-assets_ui/Assets/Scripts/PauseMenu.cs
@@ -4,23 +4,37 @@ using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
+    // Determines if the game is paused or not.
+    private bool gamePaused;
     private void Start() {
-        Debug.Log("Test");
+        gamePaused = false;
     }
     private void Update() {
-        Debug.Log("yo");    
+        // Pause the game when the user press the Cancel button.
+        if (Input.GetButtonDown("Cancel")){
+            if (gamePaused){
+                Resume();
+            } else{
+                Pause();
+            }
+           
+        }
     }
     /// <summary>
     /// Pause the game.
     /// </summary>
     public void Pause(){
-
+        gamePaused = true;
+        Time.timeScale = 0;
+        this.GetComponent<Canvas>().enabled  = true;
     }
     /// <summary>
     /// Resume the game.
     /// </summary>
     public void Resume(){
-        
+        gamePaused = false;
+        Time.timeScale = 1;
+        this.GetComponent<Canvas>().enabled  = false;
     }
     
 }
