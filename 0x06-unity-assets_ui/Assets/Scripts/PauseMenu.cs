46bd4a4a38b43b43841bb1a4d43774077b93c415
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-09-22 20:01:15 +0100

    Pausemenu

---
+++ 0x06-unity-assets_ui/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    private void Start() {
+        Debug.Log("Test");
+    }
+    private void Update() {
+        Debug.Log("yo");    
+    }
+    /// <summary>
+    /// Pause the game.
+    /// </summary>
+    public void Pause(){
+
+    }
+    /// <summary>
+    /// Resume the game.
+    /// </summary>
+    public void Resume(){
+        
+    }
+    
+}
