b0a7aea3a23f62129d86bfda0f8925b0e966f498
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-10-16 17:35:47 +0100

    New files

---
+++ 0x07-unity-animation/Assets/Scripts/CutsceneController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutsceneController : MonoBehaviour
+{
+    // Player GameObject
+    private GameObject player;
+    // TimerCanvas GameObject
+    public GameObject timercanvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    /// <summary>
+    /// Start game after animation
+    /// </summary>
+    public void startGame(){
+        player.GetComponent<PlayerController>().enabled = true;
+        player.gameObject.transform.GetChild(0).gameObject.SetActive(true);
+        timercanvas.SetActive(true);
+        this.gameObject.SetActive(false);
+    }
+}
