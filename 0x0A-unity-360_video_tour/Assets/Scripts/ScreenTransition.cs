28ba9723b61c70babf4feab1657757fa5cb63e89
Taieb Chaâbini <taiebchaabini@gmail.com>
2021-01-08 23:47:03 +0100

    360 video first commit, transition effect in progress

---
+++ 0x0A-unity-360_video_tour/Assets/Scripts/ScreenTransition.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreenTransition : MonoBehaviour
+{
+    // Camera on which we will apply our fade
+    public GameObject mainCamera;
+    // Used for blackscreen transition
+    private GameObject image;
+    // Animation component and clip used for the fadeinout effect
+    private Animation customAnimation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       image = transform.Find("Image").gameObject;
+       customAnimation = image.GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Updates the UI position and rotation according to camera's values.
+        transform.localRotation = mainCamera.transform.rotation;
+        transform.position = mainCamera.transform.position;
+    }
+
+    public void fadeInOut()
+    {
+        image.GetComponent<Image>().enabled = true;
+        customAnimation.enabled = true;
+    }
+}
