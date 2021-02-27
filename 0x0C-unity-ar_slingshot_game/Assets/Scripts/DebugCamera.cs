9563f727e579b7b2afb13f4d01c2bf676221a066
Taieb Chaâbini <taiebchaabini@gmail.com>
2021-02-27 14:59:44 +0100

    Changing gameplay

---
+++ 0x0C-unity-ar_slingshot_game/Assets/Scripts/DebugCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugCamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var rotation = new Vector3(-Input.GetAxis("Vertical"), Input.GetAxis("Horizontal"), 0.0f);
+        transform.Rotate(rotation * 100f * Time.deltaTime);
+    }
+
+
+}
